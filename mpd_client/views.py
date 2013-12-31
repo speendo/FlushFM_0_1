894bae642ab65e81c58cdf729cd60d662a83bf84
Marcel Jira <marcel@notebook-marcel>
2013-12-31 16:27:19 +0100

    also .m3u files now working

--- mpd_client/views.py
+++ mpd_client/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mpd import MPDClient
+
+import urllib.request, urllib.parse
     
 # mpd instance variable
 client = MPDClient()
@@ -16,15 +18,32 @@ def play(request):
     uri = request.POST['uri']
     if 'play' in request.POST:
         print("Play-button pressed")
-        start_replay(uri)
+        start_replay(get_uri(uri))
     else:
         print("Stop-button pressed")
         stop_repay()
     return render(request, 'mpd_client/index.html', {'uri': uri})
 
+def get_uri(uri):
+    print(uri)
+    if uri.endswith(".m3u"):
+                
+        file = urllib.request.urlopen(uri)
+        for line in file:
+            line = line.decode(encoding='utf8')
+            print(line)
+            if line.startswith('http'):
+                word = line.strip()
+                print("Word: " + word)
+                print("Ok, returning " + word)
+                return word
+    else:
+        return uri
+    
+
 def start_replay(uri):
-    print("access start_replay()")
     client.clear()
+    print("Sending \"" + uri + "\" to the client.")
     client.add(uri)
     client.play()
 
